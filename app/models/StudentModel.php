b436f60cfc9d87a241edd75e8f3df5c5be5e8df7
Longlapnghiep25 <letuanlong2908@gmail.com>
2026-05-15 22:58:28 +0700

    update project

---
+++ app/models/StudentModel.php
@@ -0,0 +1,25 @@
+<?php
+
+class StudentModel
+{
+    public function getStudents()
+    {
+        return [
+            [
+                'id' => 'SV001',
+                'name' => 'Nguyen Van A',
+                'class' => 'CNTT1'
+            ],
+            [
+                'id' => 'SV002',
+                'name' => 'Tran Thi B',
+                'class' => 'CNTT2'
+            ],
+            [
+                'id' => 'SV003',
+                'name' => 'Le Van C',
+                'class' => 'CNTT3'
+            ]
+        ];
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
-    require_once '../app/core/App.php' ;
-    $app = new App() ;
-?>
+
+require_once "./app/core/App.php";
+
+$app = new App();
